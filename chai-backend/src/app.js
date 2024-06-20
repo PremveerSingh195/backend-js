e651ff2d8512133b274be6363bc16f1b0afeff32
callsmasterofficial <partnerchc@outlook.com>
2024-06-20 10:40:42 +0530

    dcf

--- chai-backend/src/app.js
+++ chai-backend/src/app.js
@@ -13,8 +13,16 @@ app.use(cors({
 app.use(express.json({ limit: "16kb" }))
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
-
 app.use(cookieParser())
+
+//routes
+
+import UserRouter from "./routes/user.routes.js"
+
+
+//routes declaration
+
+app.use("/users", UserRouter)
 
 
 export { app }
