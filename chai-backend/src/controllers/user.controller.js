77b55f34063902b5883e79b3ea59411ee068b3f5
callsmasterofficial <partnerchc@outlook.com>
2024-06-25 16:33:19 +0530

    sxdgv

--- chai-backend/src/controllers/user.controller.js
+++ chai-backend/src/controllers/user.controller.js
@@ -160,7 +160,28 @@ const loginUser = asyncHandler(async (req, res) => {
 
 
 const logoutuser = asyncHandler(async (req, res) => {
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {
+                refreshToken: undefined
+            }
+        },
+        {
+            new: true
+        }
+    )
 
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+        .status(200)
+        .clearCookie("accessToken", options)
+        .clearCookie("refreshToken", options)
+        .json(new ApiResponse(200, {}, "user logout successfully"))
 })
 
 export { registerUser, loginUser, logoutuser }
